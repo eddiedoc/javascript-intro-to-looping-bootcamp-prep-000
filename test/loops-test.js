fe06de8f254d8c64d3d6b49048d4083b92ae6218
eddie <psyck_o@hotmail.com>
2018-01-03 06:58:01 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -41,7 +41,7 @@ describe('loops', () => {
       }
     })
   })
-/*
+
   describe('whileLoop(n)', () => {
     it('counts down from n to 0', () => {
       const spy = chai.spy.on(console, 'log')
@@ -53,7 +53,7 @@ describe('loops', () => {
       console.log.reset()
     })
   })
-*/
+/*
   describe('doWhileLoop(array)', () => {
     it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
       const [array, t] = makeArray()
@@ -63,5 +63,5 @@ describe('loops', () => {
 
       expect(newArray).to.have.length.of.at.most(l - 1)
     })
-  })
+  })*/
 })
