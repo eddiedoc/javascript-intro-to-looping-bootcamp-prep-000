eb75546220905d9b4bbea98754340a938b95b73d
eddie <psyck_o@hotmail.com>
2018-01-03 06:58:33 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -56,7 +56,7 @@ describe('loops', () => {
 
   describe('doWhileLoop(array)', () => {
     it('removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`', () => {
-      const [array, t] = makeArray()
+    //  const [array, t] = makeArray()
       const l = array.length
 
       const newArray = doWhileLoop(array)
